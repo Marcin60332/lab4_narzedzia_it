cca494c46fe2892cdc60977b367f2783dcb5b427
Marcin Kozanecki. <m_kozanecki@wp.pl>
2026-06-05 21:10:58 +0200

    dodano krótkie flagi

--- skrypt.sh
+++ skrypt.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-if [ "$1" == "--date" ]; then
+if [ "$1" == "--date" ] || [ "$1" == "-d" ]; then
 	date
 
-elif [ "$1" == "--logs" ]; then
+elif [ "$1" == "--logs" ] || [ "$1" == "-l" ]; then
 	count=100
 	if [ -n "$2" ] && [[ "$2" =~ ^[0-9]+$ ]]; then
 		count=$2
@@ -15,11 +15,11 @@ elif [ "$1" == "--logs" ]; then
 		echo "Data: $(date)" >> "$filename"
 	done
 	echo "Utworzono $count plików"
-elif [ "$1" == "--help" ]; then
+elif [ "$1" == "--help" ] || [ "$1" == "-h" ]; then
 	echo "Dostępe opcje dla skryptu: "
-	echo "Wyświetlanie dzisiejszej daty: '--date'"
-	echo "Tworzy automatycznie 100 plików log: '--logs'"
-	echo "Tworzy automatycznie podaną ilość plików log: '--logs N'"
-	echo "Wyświetla dostępne opcje skryptu: '--help'"
+	echo "Wyświetlanie dzisiejszej daty: '--date' lub '-d'"
+	echo "Tworzy automatycznie 100 plików log: '--logs' lub '-l'"
+	echo "Tworzy automatycznie podaną ilość plików log: '--logs N' lub '-l N'"
+	echo "Wyświetla dostępne opcje skryptu: '--help' lub '-h'"
 
 fi
