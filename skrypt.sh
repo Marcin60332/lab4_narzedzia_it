5625a880945176a2340539f27670810cb3d4b667
Marcin Kozanecki. <m_kozanecki@wp.pl>
2026-06-05 20:31:05 +0200

    Dodanie do skryptu tworzenie 100 plików log

--- skrypt.sh
+++ skrypt.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 if [ "$1" == "--date" ]; then
 	date
+
+elif [ "$1" == "--logs" ]; then
+	for ((i=1; i<=100; i++)); do
+		filename="log${i}.txt"
+		echo "Nazwa pliku:  $filename" > "$filename"
+		echo "Skrypt: $0" >> "$filename"
+		echo "Data: $(date)" >> "$filename"
+	done
+	echo "Utworzono 100 plików"
 fi
